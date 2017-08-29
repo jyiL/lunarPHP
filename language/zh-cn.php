e472765bdc6afcfc4b1673e7c4632065f0b21a1e
jianyi.lai <sal597685816@live.cn>
2017-08-29 16:11:25 +0800

    修改语言文件

---
+++ language/zh-cn.php
@@ -0,0 +1,333 @@
+<?php
+
+namespace LunarPHP;
+
+/**
+ * 系统语言文件.
+ * @Author: ljy
+ * @Date: 17-08-18
+ */
+
+$lang['home_contactHint'] = "<div style='text-align:justify'>感谢您对本网站的支持,若在使用上有任何问题或建议,请您不吝指教。我们会诚恳地接受并作为未来改善的重要参考。<br><br>现在,请稍加说明您的意见或您遇到的问题:</div>";
+$lang['home_contactComplete'] = "感谢您的联系!<br><br>我们会尽速处理,使服务至臻完善。";
+$lang['home_date'] = '国历(西元)';
+$lang['home_originDate'] = '国历';
+$lang['home_convertDate'] = '农历';
+$lang['home_destiny'] = '本命卦';
+$lang['home_divine'] = '卜卦';
+$lang['home_hour1'] = '子时';
+$lang['home_hour2'] = '丑时';
+$lang['home_hour3'] = '寅时';
+$lang['home_hour4'] = '卯时';
+$lang['home_hour5'] = '辰时';
+$lang['home_hour6'] = '巳时';
+$lang['home_hour7'] = '午时';
+$lang['home_hour8'] = '未时';
+$lang['home_hour9'] = '申时';
+$lang['home_hour10'] = '酉时';
+$lang['home_hour11'] = '戌时';
+$lang['home_hour12'] = '亥时';
+$lang['home_animal1'] = '青龙';
+$lang['home_animal2'] = '朱雀';
+$lang['home_animal3'] = '勾陈';
+$lang['home_animal4'] = '腾蛇';
+$lang['home_animal5'] = '白虎';
+$lang['home_animal6'] = '玄武';
+$lang['home_sky1'] = '甲';
+$lang['home_sky2'] = '乙';
+$lang['home_sky3'] = '丙';
+$lang['home_sky4'] = '丁';
+$lang['home_sky5'] = '戊';
+$lang['home_sky6'] = '己';
+$lang['home_sky7'] = '庚';
+$lang['home_sky8'] = '辛';
+$lang['home_sky9'] = '壬';
+$lang['home_sky10'] = '癸';
+$lang['home_ground1'] = '子';
+$lang['home_ground2'] = '丑';
+$lang['home_ground3'] = '寅';
+$lang['home_ground4'] = '卯';
+$lang['home_ground5'] = '辰';
+$lang['home_ground6'] = '巳';
+$lang['home_ground7'] = '午';
+$lang['home_ground8'] = '未';
+$lang['home_ground9'] = '申';
+$lang['home_ground10'] = '酉';
+$lang['home_ground11'] = '戌';
+$lang['home_ground12'] = '亥';
+$lang['home_broken'] = '⚠破';
+$lang['home_empty'] = '&curren;空亡';
+$lang['home_treat'] = '<世>';
+$lang['home_response'] = '<应>';
+$lang['home_property1'] = '金';
+$lang['home_property2'] = '木';
+$lang['home_property3'] = '水';
+$lang['home_property4'] = '火';
+$lang['home_property5'] = '土';
+$lang['home_groundAndProperty1'] = "子<span class='property'>(水)</span>";
+$lang['home_groundAndProperty2'] = "丑<span class='property'>(土)</span>";
+$lang['home_groundAndProperty3'] = "寅<span class='property'>(木)</span>";
+$lang['home_groundAndProperty4'] = "卯<span class='property'>(木)</span>";
+$lang['home_groundAndProperty5'] = "辰<span class='property'>(土)</span>";
+$lang['home_groundAndProperty6'] = "巳<span class='property'>(火)</span>";
+$lang['home_groundAndProperty7'] = "午<span class='property'>(火)</span>";
+$lang['home_groundAndProperty8'] = "未<span class='property'>(土)</span>";
+$lang['home_groundAndProperty9'] = "申<span class='property'>(金)</span>";
+$lang['home_groundAndProperty10'] = "酉<span class='property'>(金)</span>";
+$lang['home_groundAndProperty11'] = "戌<span class='property'>(土)</span>";
+$lang['home_groundAndProperty12'] = "亥<span class='property'>(水)</span>";
+$lang['home_family1'] = '父母';
+$lang['home_family2'] = '兄弟';
+$lang['home_family3'] = '子孙';
+$lang['home_family4'] = '妻财';
+$lang['home_family5'] = '官鬼';
+$lang['home_symbol1'] = '干为天';
+$lang['home_symbol2'] = '天山遁';
+$lang['home_symbol3'] = '风地观';
+$lang['home_symbol4'] = '火地晋';
+$lang['home_symbol5'] = '天风姤';
+$lang['home_symbol6'] = '天地否';
+$lang['home_symbol7'] = '山地剥';
+$lang['home_symbol8'] = '火天大有';
+$lang['home_symbol9'] = '兑为泽';
+$lang['home_symbol10'] = '泽地萃';
+$lang['home_symbol11'] = '水山蹇';
+$lang['home_symbol12'] = '雷山小过';
+$lang['home_symbol13'] = '泽水困';
+$lang['home_symbol14'] = '泽山咸';
+$lang['home_symbol15'] = '地山谦';
+$lang['home_symbol16'] = '雷泽归妹';
+$lang['home_symbol17'] = '离为火';
+$lang['home_symbol18'] = '火风鼎';
+$lang['home_symbol19'] = '山水蒙';
+$lang['home_symbol20'] = '天水讼';
+$lang['home_symbol21'] = '火山旅';
+$lang['home_symbol22'] = '火水未济';
+$lang['home_symbol23'] = '风水涣';
+$lang['home_symbol24'] = '天火同人';
+$lang['home_symbol25'] = '震为雷';
+$lang['home_symbol26'] = '雷水解';
+$lang['home_symbol27'] = '地风升';
+$lang['home_symbol28'] = '泽风大过';
+$lang['home_symbol29'] = '雷地豫';
+$lang['home_symbol30'] = '雷风恒';
+$lang['home_symbol31'] = '水风井';
+$lang['home_symbol32'] = '泽雷随';
+$lang['home_symbol33'] = '巽为风';
+$lang['home_symbol34'] = '风天小畜';
+$lang['home_symbol35'] = '风火家人';
+$lang['home_symbol36'] = '天雷旡妄';
+$lang['home_symbol37'] = '风雷益';
+$lang['home_symbol38'] = '山雷颐';
+$lang['home_symbol39'] = '山风蛊';
+$lang['home_symbol40'] = '火雷噬嗑';
+$lang['home_symbol41'] = '坎为水';
+$lang['home_symbol42'] = '水泽节';
+$lang['home_symbol43'] = '水雷屯';
+$lang['home_symbol44'] = '水火既济';
+$lang['home_symbol45'] = '泽火革';
+$lang['home_symbol46'] = '雷火丰';
+$lang['home_symbol47'] = '地火明夷';
+$lang['home_symbol48'] = '地水师';
+$lang['home_symbol49'] = '艮为山';
+$lang['home_symbol50'] = '山火贲';
+$lang['home_symbol51'] = '山天大畜';
+$lang['home_symbol52'] = '山泽损';
+$lang['home_symbol53'] = '火泽睽';
+$lang['home_symbol54'] = '天泽履';
+$lang['home_symbol55'] = '风泽中孚';
+$lang['home_symbol56'] = '风山渐';
+$lang['home_symbol57'] = '坤为地';
+$lang['home_symbol58'] = '地雷复';
+$lang['home_symbol59'] = '地泽临';
+$lang['home_symbol60'] = '地天泰';
+$lang['home_symbol61'] = '雷天大壮';
+$lang['home_symbol62'] = '泽天夬';
+$lang['home_symbol63'] = '水天需';
+$lang['home_symbol64'] = '水地比';
+
+$lang['home_yearError'] = '"年" 栏位请输入数字';
+$lang['home_yearRangeError'] = '"年" 栏位请输入1201 ~ 2050 之间';
+$lang['home_monthError'] = '"月" 栏位请输入数字';
+$lang['home_monthRangeError'] = '"月" 栏位请输入1 ~ 12 之间';
+$lang['home_dayError'] = '"日" 栏位请输入数字';
+$lang['home_dayRangeError'] = '"日" 栏位请输入1 ~ 31 之间';
+
+
+define('LANG',json_encode($lang));
+
+
+
+$tianGan = array("甲","乙","丙","丁","戊","己","庚","辛","壬","癸");
+
+$diZhi = array("子","丑","寅","卯","辰","巳","午","未","申","酉","戌","亥");
+
+$yearGan = array(
+    '1' =>  array('甲','己'),
+    '2' =>  array('乙','庚'),
+    '3' =>  array('丙','辛'),
+    '4' =>  array('丁','壬'),
+    '5' =>  array('戊','癸'),
+);
+
+$monthGan = array(
+    '1' =>  array(
+        '1'     =>  '丙寅',
+        '2'     =>  '丁卯',
+        '3'     =>  '戊辰',
+        '4'     =>  '己巳',
+        '5'     =>  '庚午',
+        '6'     =>  '辛未',
+        '7'     =>  '壬申',
+        '8'     =>  '癸酉',
+        '9'     =>  '甲戌',
+        '10'    =>  '乙亥',
+        '11'    =>  '丙子',
+        '12'    =>  '丁丑',
+    ),
+    '2' =>  array(
+        '1'     =>  '戊寅',
+        '2'     =>  '己卯',
+        '3'     =>  '庚辰',
+        '4'     =>  '辛巳',
+        '5'     =>  '壬午',
+        '6'     =>  '癸未',
+        '7'     =>  '甲申',
+        '8'     =>  '乙酉',
+        '9'     =>  '丙戌',
+        '10'    =>  '丁亥',
+        '11'    =>  '戊子',
+        '12'    =>  '己丑',
+    ),
+    '3' =>  array(
+        '1'     =>  '庚寅',
+        '2'     =>  '辛卯',
+        '3'     =>  '壬辰',
+        '4'     =>  '癸巳',
+        '5'     =>  '甲午',
+        '6'     =>  '乙未',
+        '7'     =>  '丙申',
+        '8'     =>  '丁酉',
+        '9'     =>  '戊戌',
+        '10'    =>  '己亥',
+        '11'    =>  '庚子',
+        '12'    =>  '辛丑',
+    ),
+    '4' =>  array(
+        '1'     =>  '壬寅',
+        '2'     =>  '癸卯',
+        '3'     =>  '甲辰',
+        '4'     =>  '乙巳',
+        '5'     =>  '丙午',
+        '6'     =>  '丁未',
+        '7'     =>  '戊申',
+        '8'     =>  '己酉',
+        '9'     =>  '庚戌',
+        '10'    =>  '辛亥',
+        '11'    =>  '壬子',
+        '12'    =>  '癸丑',
+    ),
+    '5' =>  array(
+        '1'     =>  '甲寅',
+        '2'     =>  '乙卯',
+        '3'     =>  '丙辰',
+        '4'     =>  '丁巳',
+        '5'     =>  '戊午',
+        '6'     =>  '己未',
+        '7'     =>  '庚申',
+        '8'     =>  '辛酉',
+        '9'     =>  '壬戌',
+        '10'    =>  '癸亥',
+        '11'    =>  '甲子',
+        '12'    =>  '乙丑',
+    ),
+);
+
+/** 天干地支 **/
+$dataInfo = array(
+    0x04bd8,0x04ae0,0x0a570,0x054d5,0x0d260,0x0d950,0x16554,0x056a0,0x09ad0,0x055d2,//1900-1909
+    0x04ae0,0x0a5b6,0x0a4d0,0x0d250,0x1d255,0x0b540,0x0d6a0,0x0ada2,0x095b0,0x14977,//1910-1919
+    0x04970,0x0a4b0,0x0b4b5,0x06a50,0x06d40,0x1ab54,0x02b60,0x09570,0x052f2,0x04970,//1920-1929
+    0x06566,0x0d4a0,0x0ea50,0x06e95,0x05ad0,0x02b60,0x186e3,0x092e0,0x1c8d7,0x0c950,//1930-1939
+    0x0d4a0,0x1d8a6,0x0b550,0x056a0,0x1a5b4,0x025d0,0x092d0,0x0d2b2,0x0a950,0x0b557,//1940-1949
+    0x06ca0,0x0b550,0x15355,0x04da0,0x0a5b0,0x14573,0x052b0,0x0a9a8,0x0e950,0x06aa0,//1950-1959
+    0x0aea6,0x0ab50,0x04b60,0x0aae4,0x0a570,0x05260,0x0f263,0x0d950,0x05b57,0x056a0,//1960-1969
+    0x096d0,0x04dd5,0x04ad0,0x0a4d0,0x0d4d4,0x0d250,0x0d558,0x0b540,0x0b6a0,0x195a6,//1970-1979
+    0x095b0,0x049b0,0x0a974,0x0a4b0,0x0b27a,0x06a50,0x06d40,0x0af46,0x0ab60,0x09570,//1980-1989
+    0x04af5,0x04970,0x064b0,0x074a3,0x0ea50,0x06b58,0x055c0,0x0ab60,0x096d5,0x092e0,//1990-1999
+    0x0c960,0x0d954,0x0d4a0,0x0da50,0x07552,0x056a0,0x0abb7,0x025d0,0x092d0,0x0cab5,//2000-2009
+    0x0a950,0x0b4a0,0x0baa4,0x0ad50,0x055d9,0x04ba0,0x0a5b0,0x15176,0x052b0,0x0a930,//2010-2019
+    0x07954,0x06aa0,0x0ad50,0x05b52,0x04b60,0x0a6e6,0x0a4e0,0x0d260,0x0ea65,0x0d530,//2020-2029
+    0x05aa0,0x076a3,0x096d0,0x04bd7,0x04ad0,0x0a4d0,0x1d0b6,0x0d250,0x0d520,0x0dd45,//2030-2039
+    0x0b5a0,0x056d0,0x055b2,0x049b0,0x0a577,0x0a4b0,0x0aa50,0x1b255,0x06d20,0x0ada0,//2040-2049
+    0x14b63,0x09370,0x049f8,0x04970,0x064b0,0x168a6,0x0ea50, 0x06b20,0x1a6c4,0x0aae0,//2050-2059
+    0x0a2e0,0x0d2e3,0x0c960,0x0d557,0x0d4a0,0x0da50,0x05d55,0x056a0,0x0a6d0,0x055d4,//2060-2069
+    0x052d0,0x0a9b8,0x0a950,0x0b4a0,0x0b6a6,0x0ad50,0x055a0,0x0aba4,0x0a5b0,0x052b0,//2070-2079
+    0x0b273,0x06930,0x07337,0x06aa0,0x0ad50,0x14b55,0x04b60,0x0a570,0x054e4,0x0d160,//2080-2089
+    0x0e968,0x0d520,0x0daa0,0x16aa6,0x056d0,0x04ae0,0x0a9d4,0x0a2d0,0x0d150,0x0f252,//2090-2099
+    0x0d520
+);
+
+$animals = array("鼠","牛","虎","兔","龙","蛇","马","羊","猴","鸡","狗","猪");
+
+$lunarInfo = array(
+    array(0,2,9,21936),array(6,1,30,9656),array(0,2,17,9584),array(0,2,6,21168),array(5,1,26,43344),array(0,2,13,59728),
+    array(0,2,2,27296),array(3,1,22,44368),array(0,2,10,43856),array(8,1,30,19304),array(0,2,19,19168),array(0,2,8,42352),
+    array(5,1,29,21096),array(0,2,16,53856),array(0,2,4,55632),array(4,1,25,27304),array(0,2,13,22176),array(0,2,2,39632),
+    array(2,1,22,19176),array(0,2,10,19168),array(6,1,30,42200),array(0,2,18,42192),array(0,2,6,53840),array(5,1,26,54568),
+    array(0,2,14,46400),array(0,2,3,54944),array(2,1,23,38608),array(0,2,11,38320),array(7,2,1,18872),array(0,2,20,18800),
+    array(0,2,8,42160),array(5,1,28,45656),array(0,2,16,27216),array(0,2,5,27968),array(4,1,24,44456),array(0,2,13,11104),
+    array(0,2,2,38256),array(2,1,23,18808),array(0,2,10,18800),array(6,1,30,25776),array(0,2,17,54432),array(0,2,6,59984),
+    array(5,1,26,27976),array(0,2,14,23248),array(0,2,4,11104),array(3,1,24,37744),array(0,2,11,37600),array(7,1,31,51560),
+    array(0,2,19,51536),array(0,2,8,54432),array(6,1,27,55888),array(0,2,15,46416),array(0,2,5,22176),array(4,1,25,43736),
+    array(0,2,13,9680),array(0,2,2,37584),array(2,1,22,51544),array(0,2,10,43344),array(7,1,29,46248),array(0,2,17,27808),
+    array(0,2,6,46416),array(5,1,27,21928),array(0,2,14,19872),array(0,2,3,42416),array(3,1,24,21176),array(0,2,12,21168),
+    array(8,1,31,43344),array(0,2,18,59728),array(0,2,8,27296),array(6,1,28,44368),array(0,2,15,43856),array(0,2,5,19296),
+    array(4,1,25,42352),array(0,2,13,42352),array(0,2,2,21088),array(3,1,21,59696),array(0,2,9,55632),array(7,1,30,23208),
+    array(0,2,17,22176),array(0,2,6,38608),array(5,1,27,19176),array(0,2,15,19152),array(0,2,3,42192),array(4,1,23,53864),
+    array(0,2,11,53840),array(8,1,31,54568),array(0,2,18,46400),array(0,2,7,46752),array(6,1,28,38608),array(0,2,16,38320),
+    array(0,2,5,18864),array(4,1,25,42168),array(0,2,13,42160),array(10,2,2,45656),array(0,2,20,27216),array(0,2,9,27968),
+    array(6,1,29,44448),array(0,2,17,43872),array(0,2,6,38256),array(5,1,27,18808),array(0,2,15,18800),array(0,2,4,25776),
+    array(3,1,23,27216),array(0,2,10,59984),array(8,1,31,27432),array(0,2,19,23232),array(0,2,7,43872),array(5,1,28,37736),
+    array(0,2,16,37600),array(0,2,5,51552),array(4,1,24,54440),array(0,2,12,54432),array(0,2,1,55888),array(2,1,22,23208),
+    array(0,2,9,22176),array(7,1,29,43736),array(0,2,18,9680),array(0,2,7,37584),array(5,1,26,51544),array(0,2,14,43344),
+    array(0,2,3,46240),array(4,1,23,46416),array(0,2,10,44368),array(9,1,31,21928),array(0,2,19,19360),array(0,2,8,42416),
+    array(6,1,28,21176),array(0,2,16,21168),array(0,2,5,43312),array(4,1,25,29864),array(0,2,12,27296),array(0,2,1,44368),
+    array(2,1,22,19880),array(0,2,10,19296),array(6,1,29,42352),array(0,2,17,42208),array(0,2,6,53856),array(5,1,26,59696),
+    array(0,2,13,54576),array(0,2,3,23200),array(3,1,23,27472),array(0,2,11,38608),array(11,1,31,19176),array(0,2,19,19152),
+    array(0,2,8,42192),array(6,1,28,53848),array(0,2,15,53840),array(0,2,4,54560),array(5,1,24,55968),array(0,2,12,46496),
+    array(0,2,1,22224),array(2,1,22,19160),array(0,2,10,18864),array(7,1,30,42168),array(0,2,17,42160),array(0,2,6,43600),
+    array(5,1,26,46376),array(0,2,14,27936),array(0,2,2,44448),array(3,1,23,21936),array(0,2,11,37744),array(8,2,1,18808),
+    array(0,2,19,18800),array(0,2,8,25776),array(6,1,28,27216),array(0,2,15,59984),array(0,2,4,27424),array(4,1,24,43872),
+    array(0,2,12,43744),array(0,2,2,37600),array(3,1,21,51568),array(0,2,9,51552),array(7,1,29,54440),array(0,2,17,54432),
+    array(0,2,5,55888),array(5,1,26,23208),array(0,2,14,22176),array(0,2,3,42704),array(4,1,23,21224),array(0,2,11,21200),
+    array(8,1,31,43352),array(0,2,19,43344),array(0,2,7,46240),array(6,1,27,46416),array(0,2,15,44368),array(0,2,5,21920),
+    array(4,1,24,42448),array(0,2,12,42416),array(0,2,2,21168),array(3,1,22,43320),array(0,2,9,26928),array(7,1,29,29336),
+    array(0,2,17,27296),array(0,2,6,44368),array(5,1,26,19880),array(0,2,14,19296),array(0,2,3,42352),array(4,1,24,21104),
+    array(0,2,10,53856),array(8,1,30,59696),array(0,2,18,54560),array(0,2,7,55968),array(6,1,27,27472),array(0,2,15,22224),
+    array(0,2,5,19168),array(4,1,25,42216),array(0,2,12,42192),array(0,2,1,53584),array(2,1,21,55592),array(0,2,9,54560)
+);
+
+$hour = array(
+    '子时' =>  1,    // (23 ~ 1)
+    '丑时' =>  2,    // (1 ~ 3)
+    '寅时' =>  3,    // (3 ~ 5)
+    '卯时' =>  4,    // (5 ~ 7)
+    '辰时' =>  5,    // (7 ~ 9)
+    '巳时' =>  6,    // (9 ~ 11)
+    '午时' =>  7,    // (11 ~ 13)
+    '未时' =>  8,    // (13 ~ 15)
+    '申时' =>  9,    // (15 ~ 17)
+    '酉时' =>  10,    // (17 ~ 19)
+    '戌时' =>  11,    // (19 ~ 21)
+    '亥时' =>  12,    // (21 ~ 23)
+);
+
+define('YEARGAN',json_encode($yearGan));    // 六十甲子年
+define('MONTHGAN',json_encode($monthGan));    // 六十甲子月
+define('TIANGAN',json_encode($tianGan));    // 十天干
+define('DIZHI',json_encode($diZhi));    // 十二地支
+define('DATAINFO',json_encode($dataInfo));    // 农历十六进制数据
+define('ANIMALS',json_encode($animals));    // 十二生肖
+define('LUNARINFO',json_encode($lunarInfo));
+define('HOUR',json_encode($hour));    // 时辰
