a79859a0bcb175a7d168bf8c693f0e0cbc18a57e
jianyi.lai <sal597685816@live.cn>
2017-08-29 16:11:59 +0800

    修改类库入口文件

--- lunarPHP.php
+++ lunarPHP.php
@@ -8,6 +8,6 @@ include_once __DIR__.'/config/config.php';
 //引入自动载入函数
 include_once __DIR__.'/autoloader.php';
 //引入语言文件
-include_once __DIR__.'/language/language.php';
+include_once __DIR__.'/language/zh-cn.php';
 //调用自动载入函数
 AutoLoader::register();
