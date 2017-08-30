6edec40c2319cae743da8651b733892eae90ca59
jianyi.lai <sal597685816@live.cn>
2017-08-30 11:24:10 +0800

    修改类库版本号

--- config/config.php
+++ config/config.php
@@ -37,5 +37,5 @@ define('SITE_PATH', dirname(dirname(__FILE__)) . "/");
 
 
 //版本号
-define('LUNARPHP_VERSION', '1.0');
+define('LUNARPHP_VERSION', '1.5');
 define('LUNARPHP_VERSION_DATE', '2017-08-18');
